96ba5ebd873b03388c65d524072c5b02bbf26d55
vikas1yadav <39706916+vikas1yadav@users.noreply.github.com>
2020-06-26 19:33:24 +0530

    Update BidderData.sol

--- BidderData.sol
+++ BidderData.sol
@@ -2,10 +2,22 @@ pragma solidity ^0.4.0;
 
 contract Bidder {
     
-     string public name;
-     uint public bidAmount = 20000;
+     string public name = "vikas";
+     uint public bidAmount;
      bool public  eligible;
      uint constant minBid = 1000;
      
+     function setName(string nm) public {
+         name = nm;
+     }
+     
+     function setBidAmount(uint x) public {
+         bidAmount=x;
+     }
+     
+     function determineEligibility() public {
+         if (bidAmount >= minBid) eligible = true;
+         else eligible=false;
+     }
      
 }
